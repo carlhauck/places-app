daffc3dbed41a4b0cbb29513f469eb8d78b47352
carlhauck <carlhauck@gmail.com>
2020-07-06 22:53:49 -0500

    adds Place model and RESTful backend routes

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'ffaker'
+
+10.times do
+ Place.create(name: FFaker::Name.name, address: FFaker::Address.street_address(include_secondary = false))
+end
